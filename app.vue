c442e5f9b39328160e5891ef4e0d5df2965c1733
RyuseiNomi <kanontonto1010@gmail.com>
2024-12-16 22:06:46 +0900

    create a component to input race result

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="score-maker-body">
     <MemberInput />
     <CardSelect />
+    <RaceResultInput />
   </div>
 </template>
+
+<style scoped>
+.score-maker-body {
+  padding: 0 50px;
+}
+</style>
---
+++ components/RaceResultInput.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+const items = [
+  {
+    slot: 'first',
+    label: '1st Race'
+  },
+  {
+    slot: 'second',
+    label: '2nd Race'
+  },
+  {
+    slot: 'third',
+    label: '3rd Race'
+  },
+  {
+    slot: 'fourth',
+    label: '4th Race'
+  },
+  {
+    slot: 'fifth',
+    label: '5th Race'
+  },
+  {
+    slot: 'sixth',
+    label: '6th Race'
+  },
+  {
+    slot: 'seventh',
+    label: '7th Race'
+  },
+  {
+    slot: 'eighth',
+    label: '8th Race'
+  },
+]
+
+</script>
+
+<template>
+  <UTabs :items="items" class="w-full">
+    <template #first="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+    <template #second="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+    <template #third="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+    <template #fourth="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+    <template #fifth="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+    <template #sixth="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+    <template #seventh="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+    <template #eighth="{ item }">
+      <UCard>
+        <template #header>
+          <p>{{ item.label }}</p>
+        </template>
+      </UCard>
+    </template>
+  </UTabs>
+</template>
