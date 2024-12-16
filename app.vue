b791d94d1409f7e441b57ceab84d3d6f01688513
RyuseiNomi <kanontonto1010@gmail.com>
2024-12-16 20:28:04 +0900

    set a config to import components

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <MemberInput />
   </div>
 </template>
---
+++ components/MemberInput.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>メンバーを入力</p>
+  </div>
+</template>
