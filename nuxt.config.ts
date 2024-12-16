6358937493645cef62188f1c59e1fcc67cd8e0ce
RyuseiNomi <kanontonto1010@gmail.com>
2024-12-16 20:28:46 +0900

    install NuxtUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   components: [
     {
       path: '~/components',
       pathPrefix: false,
     },
   ],
+
+  modules: ['@nuxt/ui'],
 })
