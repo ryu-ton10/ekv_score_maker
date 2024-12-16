0b10b120d555203106bae1d349e23758060659b5
RyuseiNomi <kanontonto1010@gmail.com>
2024-12-16 21:05:04 +0900

    create member input component

--- components/MemberInput.vue
+++ components/MemberInput.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
     <p>メンバーを入力</p>
+    <div class="member-input-table">
+      <div>
+        <p>Aチーム</p>
+        <UInput />
+        <UInput />
+        <UInput />
+      </div>
+      <div>
+        <p>Bチーム</p>
+        <UInput />
+        <UInput />
+        <UInput />
+      </div>
+      <div>
+        <p>Cチーム</p>
+        <UInput />
+        <UInput />
+        <UInput />
+      </div>
+      <div>
+        <p>Dチーム</p>
+        <UInput />
+        <UInput />
+        <UInput />
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.member-input-table {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 10px;
+}
+</style>
