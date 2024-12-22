2fd1a92b25f5fc89687b8f103e716caea4090f62
RyuseiNomi <kanontonto1010@gmail.com>
2024-12-22 21:34:19 +0900

    add function to select team

--- pages/member/index.vue
+++ pages/member/index.vue
@@ -1,14 +1,38 @@
 <template>
   <div>
-    <p>メンバーを入力</p>
-    <div class="member-input-table">
+    <div class="input-table">
+      <UForm :schema="teamNameSchema" :state="teamName" @submit="addTeamName">
+        <p>チーム名を入力</p>
+        <UFormGroup name="teamName">
+          <UInput class="input" v-model="teamName.name" />
+        </UFormGroup>
+        <UButton type="submit">追加</UButton>
+      </UForm>
+      <div>
+        <div v-for="teamName in teamNames">
+          <p>{{ teamName.name }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="input-table">
       <UForm :schema="memberSchema" :state="member" @submit="addMember">
-        <p>Aチーム</p>
+        <p>メンバーを入力</p>
+        <USelectMenu
+          v-model="selectedTeam"
+          :options="teamNames"
+          option-attribute="name"
+          placeholder="チームを選択"
+        />
         <UFormGroup name="name">
           <UInput class="input" v-model="member.name" />
         </UFormGroup>
-        <UButton type="submit">Lock</UButton>
+        <UButton type="submit">追加</UButton>
       </UForm>
+      <div>
+        <div v-for="member in members">
+          <p>{{ member.name }}</p>
+        </div>
+      </div>
     </div>
     <div class="member-show-table">
     </div>
@@ -26,34 +50,53 @@ import type { FormSubmitEvent } from '#ui/types'
 export default defineComponent({
   setup() {
     const store = useMembersStore()
+    const teamNameSchema = object(
+      { name: string() }
+    )
     const memberSchema = object(
       { name: string() }
     )
 
-    type Schema = InferType<typeof memberSchema>
+    type TeamNameSchema = InferType<typeof teamNameSchema>
+    type MemberSchema = InferType<typeof memberSchema>
 
-    function addMember(event: FormSubmitEvent<Schema>) {
+    function addTeamName(event: FormSubmitEvent<TeamNameSchema>) {
       if (!event.data.name) { return; }
-      store.addMember('A', event.data.name)
-      console.log(store.members)
+      store.addTeamName(event.data.name)
     }
 
+    function addMember(event: FormSubmitEvent<MemberSchema>) {
+      if (!event.data.name) { return; }
+      store.addMember(selectedTeam.value.name, event.data.name)
+    }
+
+    const teamNames = store.rawTeamNames
+    const teamName = store.teamName
+    const selectedTeam = ref(teamNames[0])
+    const members = store.rawMembers
     const member = store.member
 
     return {
+      members,
       member,
+      teamNames,
+      teamName,
+      selectedTeam,
+      teamNameSchema,
       memberSchema,
       addMember,
+      addTeamName,
     }
   }
 })
 </script>
 
 <style scoped>
-.member-input-table {
+.input-table {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 10px;
+  padding: 30px 0;
 }
 
 .input {
--- stores/members.ts
+++ stores/members.ts
@@ -2,14 +2,21 @@ import { defineStore } from 'pinia'
 
 export const useMembersStore = defineStore('membersStore', {
   state: () => ({
+    rawTeamNames: [] as Array<{ name: string }>, 
+    teamName: {} as { name: string },
     rawMembers: [] as Array<{ team: string; name: string }>,
     member: {} as { team: string; name: string },
   }),
   getters: {
+    teamNames: (state): Array<{ team: string; name: string }> =>
+      state.rawMembers,
     members: (state): Array<{ team: string; name: string }> =>
-      state.rawMembers
+      state.rawMembers,
   },
   actions: {
+    addTeamName(name: string) {
+      this.rawTeamNames.push({ name: name })
+    },
     addMember(team: string, name: string) {
       this.rawMembers.push({ team: team, name: name })
     },
